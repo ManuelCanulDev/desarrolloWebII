5e9574d6301e41d3134d2aaf0eab3f260660bd72
ManuelCanul109 <manuel.canul109@gmail.com>
2021-03-04 17:49:34 -0500

    Parcial 1 EXAMEN

--- portal/src/routes/web.php
+++ portal/src/routes/web.php
@@ -10,15 +10,7 @@ use Illuminate\Http\Request;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
-
-/*Route::get('/iniciodesesion', function () {
-    return view('login');
-});*/
 
 Route::get('/', function () {
     return view('home');
@@ -31,7 +23,4 @@ Route::get('/makan', function () {
 Route::get('/asd', function () {
     return view('makan');
 });
-
-//Route::get('terminosycondiciones', 'App\Http\Controllers\EstaticosController@terminosycondiciones');
-//Route::post('ajaxPrelogin', 'App\Http\Controllers\LoginController@prelogin')->name('ajaxPrelogin');
 
